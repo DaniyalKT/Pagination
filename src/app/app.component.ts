7da46529617479af0048fdd96c9b49465ef140a4
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-12-06 18:01:00 +0330

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pagination';
+currentPage: number = 0
+
+
+
+  onActiveClass(index: number){
+     return (index==this.currentPage) ? 'active' : ''
+  }
+
+
+  images = [
+    {
+      title: 'this is a beach',
+      url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
+    },
+    {
+      title: 'this is a beach',
+      url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
+    },
+    {
+      title: 'this is a beach',
+      url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
+    }
+  ]
+
 }
