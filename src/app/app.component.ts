0fc74f2ed8329cf2067f258560cccd428beeee90
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-12-06 18:40:11 +0330

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,16 +17,16 @@ currentPage: number = 0
 
   images = [
     {
-      title: 'this is a beach',
+      title: 'this is a Beach',
       url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
     },
     {
-      title: 'this is a beach',
-      url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
+      title: 'this is a jungle',
+      url: 'https://www.guide-bearn-pyrenees.com/_bibli/annonces/3036/hd/picdumidiossau.jpg'
     },
     {
-      title: 'this is a beach',
-      url: 'https://img.freepik.com/free-photo/beautiful-tropical-beach-sea_74190-6796.jpg?w=2000'
+      title: 'this is a Sea',
+      url: 'https://www.timeforkids.com/wp-content/uploads/2021/07/turtle1.jpg'
     }
   ]
 
